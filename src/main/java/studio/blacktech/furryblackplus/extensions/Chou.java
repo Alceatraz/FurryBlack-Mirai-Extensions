630da34a0bdbc6ecfc5935261ca8b2fba88278d3
Alceatraz <alceatraz@blacktech.studio>
2021-08-05 15:24:12 +0800

    2.0.2

--- src/main/java/studio/blacktech/furryblackplus/extensions/Chou.java
+++ src/main/java/studio/blacktech/furryblackplus/extensions/Chou.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2021 Alceatraz @ BlackTechStudio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the BTS Anti Commercial & GNU Affero General.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * BTS Anti Commercial & GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the BTS Anti Commercial & GNU Affero
+ * General Public License along with this program in README or LICENSE.
+ */
+
 package studio.blacktech.furryblackplus.extensions;
 
 import net.mamoe.mirai.contact.ContactList;
